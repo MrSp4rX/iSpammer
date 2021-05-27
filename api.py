26e739c9e0424e7f7fb50c7fcf9c3088df75f6ff
Mr SparX <71897067+MrSp4rX@users.noreply.github.com>
2021-05-26 22:40:42 -0700

    Update api.py

--- api.py
+++ api.py
@@ -400,7 +400,7 @@ class Api():
                         # curl -X POST -H "Host:asvmfaizabad.org" -H "Connection:keep-alive" -H "Content-Length:83" -H "Cache-Control:max-age=0" -H "Upgrade-Insecure-Requests:1" -H "Origin:http://asvmfaizabad.org" -H "Content-Type:application/x-www-form-urlencoded" -H "User-Agent:Mozilla/5.0 (Linux; Android 8.1.0; CPH1909) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36" -H "Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Referer:http://asvmfaizabad.org/register.php" -H "Accept-Encoding:gzip, deflate" -H "Accept-Language:en-US,en;q=0.9,hi;q=0.8" -H "Cookie:wh-widget-cookie=1" -d 'sname=Madarchod&sclass=XII&sphone='''+target+'''&spassword=Madarchod&ssection=A&submit=' "http://asvmfaizabad.org/register.php" 
                         # ''')
 
-                        if times > msgs*1:
+                        if times > msgs*2:
                                 break
                         else:
                                 pass
